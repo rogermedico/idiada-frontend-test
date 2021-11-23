9327c077c9e5ad2dd2c38258ea327ab43cb3fd6e
rogermedico <roger.medico@gmail.com>
2021-11-23 10:57:23 +0000

    primeng table replaced with angular material table

--- src/app/modules/vehicles/views/vehicles-list/vehicles-list.component.ts
+++ src/app/modules/vehicles/views/vehicles-list/vehicles-list.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { VehiclesService } from '../../services/vehicles.service';
 import { VehicleView } from '../../../../models/vehicleView';
 import { VehicleDialogComponent } from '../dialogs/vehicle-dialog/vehicle-dialog.component';
-import { filter, take, tap } from 'rxjs/operators';
+import { filter, mergeMap, take, tap } from 'rxjs/operators';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../dialogs/confirm-dialog/confirm-dialog.component';
+import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-vehicles-list',
@@ -13,8 +15,9 @@ import { ConfirmDialogComponent } from '../dialogs/confirm-dialog/confirm-dialog
 })
 export class VehiclesListComponent implements OnInit {
 
-  vehicleList: VehicleView[] = [];
-  vehicle: VehicleView | undefined;
+  public vehicleList: MatTableDataSource<VehicleView> = new MatTableDataSource();//[] = [];
+  public vehicle: VehicleView | undefined;
+  public vehicleTableDisplayedColumns: string[] = ['id', 'plate', 'manufacturer', 'make', 'commercialName', 'vinNumber', 'capacity', 'actions']
 
   constructor(
     private vs: VehiclesService,
@@ -25,26 +28,27 @@ export class VehiclesListComponent implements OnInit {
     this.vs.getVehicles().pipe(
       take(1),
       tap((vehicles: VehicleView[]) => {
-        this.vehicleList = vehicles
+        this.vehicleList.data = vehicles
       })
     ).subscribe();
+
   }
 
   createVehicle(): void {
     const dialogRef = this.dialog.open(VehicleDialogComponent);
     dialogRef.afterClosed().pipe(
       take(1),
-      filter(vehicleData => vehicleData),
+      filter(vehicleFormData => vehicleFormData),
       tap(vehicleFormData => {
         this.vs.createVehicle(vehicleFormData).subscribe(vehicle => {
-          this.vehicleList = [...this.vehicleList, vehicle];
+          this.vehicleList.data = [...this.vehicleList.data, vehicle];
         })
       })
     ).subscribe();
   }
 
   updateVehicle(id: number): void {
-    const vehicle = this.vehicleList.find(v => v.id === id);
+    const vehicle = this.vehicleList.data.find(v => v.id === id);
     if (vehicle) {
       const dialogRef = this.dialog.open(VehicleDialogComponent, {
         data: {
@@ -66,7 +70,7 @@ export class VehiclesListComponent implements OnInit {
           };
           this.vs.updateVehicle(modifiedVehicle).pipe(
             tap(() => {
-              this.vehicleList = this.vehicleList.map((v: VehicleView) => {
+              this.vehicleList.data = this.vehicleList.data.map((v: VehicleView) => {
                 if (v.id != modifiedVehicle.id) return v;
                 else return modifiedVehicle;
               });
@@ -92,11 +96,15 @@ export class VehiclesListComponent implements OnInit {
         this.vs.deleteVehicle(vehicle.id).pipe(
           take(1),
           tap(() => {
-            this.vehicleList = this.vehicleList.filter((v: VehicleView) => v.id != vehicle.id);
+            this.vehicleList.data = this.vehicleList.data.filter((v: VehicleView) => v.id != vehicle.id);
           })
         ).subscribe();
       })).subscribe();
   }
 
+  vehiclesApplyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.vehicleList.filter = filterValue.trim().toLowerCase();
+  }
 
 }
